ba9e5eb7ec99fb1b250f41b94676be54bcc4ad7d
ignasvoveriukas9 <ignas.voveriukas123@gmail.com>
2023-12-05 11:44:28 +0200

    implemented coding

--- Kodavimo_teorija/Coder.cs
+++ Kodavimo_teorija/Coder.cs
@@ -1,14 +1,33 @@
 namespace Kodavimo_teorija;
+using System;
 
 public static class Coder
 {
     public static string code(string input)
     {
-        string output;
-        
-        //TODO inplement coding part
+        string output = input;
 
-        output = input;
+        // Example matrix
+        int[,] matrix = new int[,]
+        {
+            {1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1},
+            {1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0}, 
+            {0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0},
+            {1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1},
+            {1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0},
+            {1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0},
+            {0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0},
+            {0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0},
+            {0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1},
+            {1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0},
+            {0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1},
+            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+        };
+
+        // Multiply binary vector with matrix;
+        string multipledByMetrix = MultiplyBinaryVectorWithMatrix(input, matrix);
+
+        output += multipledByMetrix;
 
         return output;
     }
@@ -23,5 +42,27 @@ public static class Coder
         }
 
         return output;
+    }
+
+
+    static string MultiplyBinaryVectorWithMatrix(string binaryVector, int[,] matrix)
+    {
+        if (binaryVector.Length != matrix.GetLength(0))
+        {
+            throw new ArgumentException("Vector length must match matrix row count.");
+        }
+
+        int[] resultArray = new int[matrix.GetLength(1)];
+
+        for (int i = 0; i < matrix.GetLength(1); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(0); j++)
+            {
+                resultArray[i] += (binaryVector[j] - '0') * matrix[j, i];
+            }
+            resultArray[i] %= 2; // Apply modulo 2 for binary result
+        }
+
+        return string.Join("", resultArray);
     }
 }
